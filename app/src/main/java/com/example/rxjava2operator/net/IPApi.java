056a061e2c27e18e12193dd47d45bed3905bf9bc
zhongjh <254191389@qq.com>
2020-07-20 09:30:25 +0800

    增加url传参的示例

--- app/src/main/java/com/example/rxjava2operator/net/IPApi.java
+++ app/src/main/java/com/example/rxjava2operator/net/IPApi.java
@@ -1,9 +1,14 @@
 package com.example.rxjava2operator.net;
 
-import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
 
+/**
+ * https都是可以支持的
+ */
 public interface IPApi {
 
     /**
@@ -13,7 +18,12 @@ public interface IPApi {
     @GET("/")
     Observable<String> GetIP();
 
-    @GET("/")
-    Flowable<String> GetIP2();
+    /**
+     * 这种形式可以直接传url进去
+     * @param url url
+     * @param message 参数
+     */
+    @POST
+    Observable<String> GetIP2(@Url String url, @Body String message);
 
 }
